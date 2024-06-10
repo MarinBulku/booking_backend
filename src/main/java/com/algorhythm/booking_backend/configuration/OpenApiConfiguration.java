c35319b18d1b0230bd80b5e9b3c3734344ade084
MarinBulku <marinbulku4@gmail.com>
2024-06-10 16:09:18 +0200

    Implemented OpenApi and Swagger.

--- src/main/java/com/algorhythm/booking_backend/configuration/OpenApiConfiguration.java
+++ src/main/java/com/algorhythm/booking_backend/configuration/OpenApiConfiguration.java
@@ -1,8 +1,11 @@
 package com.algorhythm.booking_backend.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
@@ -20,6 +23,14 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         url = "http://localhost:8081"
                 )
         }
+)
+@SecurityScheme(
+        name = "Bearer authentication",
+        description = "Must use a token to verify authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
 }
