230f424eab87d32c4b680741eac7f1c7544e1bb2
MarinBulku <marinbulku4@gmail.com>
2024-06-10 16:07:40 +0200

    Handling the exceptions and returning proper response upon caused exceptions.

---
+++ src/main/java/com/algorhythm/booking_backend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.algorhythm.booking_backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {
+            EntityNotFoundException.class
+    })
+    public ResponseEntity<ApiException> handleNotFoundExceptions(Exception e) {
+
+
+        ApiException exceptionToBeThrown = new ApiException(
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(exceptionToBeThrown, HttpStatus.BAD_REQUEST);
+    }
+}
