aabb66b1fc818f2edb3401f390e1e21ec0095cf6
MarinBulku <marinbulku4@gmail.com>
2024-06-10 16:09:46 +0200

    Added Controllers.

---
+++ src/main/java/com/algorhythm/booking_backend/controllers/RoleController.java
@@ -0,0 +1,67 @@
+package com.algorhythm.booking_backend.controllers;
+
+import com.algorhythm.booking_backend.entities.Role;
+import com.algorhythm.booking_backend.exceptions.EntityNotFoundException;
+import com.algorhythm.booking_backend.services.interfaces.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/roles")
+@Tag(
+        name = "Roles endpoints",
+        description = "All apis of role"
+)
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Get all roles")
+    public ResponseEntity<List<Role>> getAllRoles(){
+
+        List<Role> allRoles = roleService.findAll();
+        return ResponseEntity.ok(allRoles);
+    }
+
+    @GetMapping("/role")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Get a role by its id")
+    public ResponseEntity<Role> getRoleById(@RequestParam Integer roleId){
+            Role roleFound = roleService.findById(roleId);
+            return ResponseEntity.ok(roleFound);
+    }
+
+    @PutMapping("/update")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Update a role name with its Id")
+    public ResponseEntity<Role> updateRole(@RequestParam Integer roleId, @RequestParam String roleName){
+            Role role = roleService.updateRole(roleId, roleName);
+            return ResponseEntity.ok(role);
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Create a role with a name")
+    public ResponseEntity<Role> createRole(@RequestParam String roleName){
+            Role role = roleService.createRole(roleName);
+            return ResponseEntity.ok(role);
+    }
+
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Delete a role by its Id")
+    public ResponseEntity<Void> deleteRole(@RequestParam Integer roleId){
+            roleService.deleteRoleById(roleId);
+            return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/algorhythm/booking_backend/controllers/UserController.java
@@ -0,0 +1,77 @@
+package com.algorhythm.booking_backend.controllers;
+
+import com.algorhythm.booking_backend.dataproviders.authentication.AuthenticationRequest;
+import com.algorhythm.booking_backend.dataproviders.authentication.AuthenticationResponse;
+import com.algorhythm.booking_backend.dataproviders.User.NewUserDto;
+import com.algorhythm.booking_backend.dataproviders.User.UserDto;
+import com.algorhythm.booking_backend.services.interfaces.RoleService;
+import com.algorhythm.booking_backend.services.interfaces.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Tag(
+        name = "User",
+        description = "All APIs of Users"
+)
+public class UserController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @PostMapping("/login")
+    @Operation(summary = "Authenticate and receive a token")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+            AuthenticationResponse response = userService.authenticate(request);
+            return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/logout")
+    @Operation(summary = "Deauthenticate")
+    public ResponseEntity<Void> deauthenticate(@RequestParam String token){
+        userService.deauthenticate(token);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all users")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> allUsers = userService.findAll();
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @GetMapping("/user")
+    @Operation(summary = "Get user by id")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserDto> getUserById(@RequestParam Integer userId){
+        UserDto user = userService.findById(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    @Operation(summary = "Create a new user")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserDto> createUser(@RequestBody NewUserDto newUser){
+
+        UserDto user = userService.addUser(newUser);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(summary = "Remove a user by its id")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> removeUser(@RequestParam Integer userId){
+            userService.removeUser(userId);
+            return ResponseEntity.ok(null);
+    }
+}
