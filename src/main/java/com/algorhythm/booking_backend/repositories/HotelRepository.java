bf8f837065a4d57b1c070823273887ed26c64ff6
MarinBulku <marinbulku4@gmail.com>
2024-06-19 11:33:15 +0200

    New methods added to services.

--- src/main/java/com/algorhythm/booking_backend/repositories/HotelRepository.java
+++ src/main/java/com/algorhythm/booking_backend/repositories/HotelRepository.java
@@ -1,7 +1,29 @@
 package com.algorhythm.booking_backend.repositories;
 
+import com.algorhythm.booking_backend.dataproviders.Hotel.AvailableHotelDto;
 import com.algorhythm.booking_backend.entities.Hotel;
+import com.algorhythm.booking_backend.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+    List<Hotel> findHotelsByOwner(User owner);
+
+    @Query("select distinct h from Hotel h " +
+            "inner join Room r " +
+            "on (r.hotel = h and r.adultsCapacity = :adultsNumber and r.kidsCapacity = :kidsNumber)" +
+            "where not exists (select distinct r from Room r " +
+            "inner join Booking b on r = b.room " +
+            "where ((b.startDate < :checkInDate and :checkInDate < b.endDate) or ((b.startDate < :checkOutDate and :checkOutDate < b.endDate))))")
+    Page<Hotel> findAvailableHotels(
+            LocalDate checkInDate,
+            LocalDate checkOutDate,
+            Integer adultsNumber,
+            Integer kidsNumber,
+            Pageable pageable);
 }
--- src/main/java/com/algorhythm/booking_backend/services/interfaces/HotelService.java
+++ src/main/java/com/algorhythm/booking_backend/services/interfaces/HotelService.java
@@ -1,9 +1,11 @@
 package com.algorhythm.booking_backend.services.interfaces;
 
+import com.algorhythm.booking_backend.dataproviders.Booking.BookingRequest;
+import com.algorhythm.booking_backend.dataproviders.Hotel.AvailableHotelDto;
 import com.algorhythm.booking_backend.dataproviders.Hotel.HotelCreationRequest;
 import com.algorhythm.booking_backend.dataproviders.Hotel.HotelDTO;
 import com.algorhythm.booking_backend.entities.Hotel;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -12,6 +14,10 @@ public interface HotelService {
     List<Hotel> findAll();
 
     List<HotelDTO> allHotelDtos();
+
+    List<HotelDTO> allHotelDtosByOwner(Integer ownerId);
+
+    Page<AvailableHotelDto> findAllAvailableHotels(BookingRequest request, Integer pageNo) throws Exception;
 
     Hotel findById(Integer id);
 
