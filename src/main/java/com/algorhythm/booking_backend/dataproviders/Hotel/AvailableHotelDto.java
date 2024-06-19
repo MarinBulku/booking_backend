d035795e1dc6013766695bddf93ce17d81f04d1f
MarinBulku <marinbulku4@gmail.com>
2024-06-19 11:30:10 +0200

    Added private access modifiers.

---
+++ src/main/java/com/algorhythm/booking_backend/dataproviders/Hotel/AvailableHotelDto.java
@@ -0,0 +1,21 @@
+package com.algorhythm.booking_backend.dataproviders.Hotel;
+
+import jakarta.persistence.Column;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AvailableHotelDto {
+    private Integer hotelId;
+    private String hotelName;
+    private MultipartFile hotelImage;
+    private boolean freeParking;
+    private boolean freeWiFi;
+    private boolean freePool;
+    private boolean freeBreakfast;
+}
