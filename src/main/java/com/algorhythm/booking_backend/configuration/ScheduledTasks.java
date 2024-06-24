43d52c65e5b04265e7c52c2e9f302ed84958ef78
MarinBulku <marinbulku4@gmail.com>
2024-06-24 15:45:05 +0200

    A scheduled task to update the bookings to complete.

--- src/main/java/com/algorhythm/booking_backend/configuration/ScheduledTasks.java
+++ src/main/java/com/algorhythm/booking_backend/configuration/ScheduledTasks.java
@@ -1,10 +1,12 @@
 package com.algorhythm.booking_backend.configuration;
 
+import com.algorhythm.booking_backend.repositories.BookingRepository;
 import com.algorhythm.booking_backend.repositories.RevokedTokensRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Component
@@ -12,11 +14,17 @@ import java.time.LocalDateTime;
 public class ScheduledTasks {
 
     private final RevokedTokensRepository revokedTokensRepository;
+    private final BookingRepository bookingRepository;
 
     @Scheduled(cron = "0 0 3 * * *")
     public void deleteRevokedTokens(){
         LocalDateTime time = LocalDateTime.now().minusHours(8);
         revokedTokensRepository.deleteOlderThan(time);
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void updateBookingStatus(){
+        bookingRepository.updateCompletedBookings(LocalDate.now());
     }
 
 }
