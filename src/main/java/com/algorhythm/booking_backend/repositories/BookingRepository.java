d853f85b11d42cb159a53548d66a9912b7c27ae8
MarinBulku <marinbulku4@gmail.com>
2024-06-24 15:44:01 +0200

    New method to set booking status to complete.

--- src/main/java/com/algorhythm/booking_backend/repositories/BookingRepository.java
+++ src/main/java/com/algorhythm/booking_backend/repositories/BookingRepository.java
@@ -2,11 +2,14 @@ package com.algorhythm.booking_backend.repositories;
 
 import com.algorhythm.booking_backend.dataproviders.Booking.BookingHistoryDto;
 import com.algorhythm.booking_backend.entities.Booking;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
@@ -14,4 +17,9 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     @Query("SELECT NEW com.algorhythm.booking_backend.dataproviders.Booking.BookingHistoryDto(b.room.hotel.hotelName, b.pricePaid, b.startDate, b.status) " +
             "FROM Booking b WHERE b.user.userId = :userId")
     Page<BookingHistoryDto> findByUser_UserId(Integer userId, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Booking b SET b.status = 'COMPLETE' WHERE b.status = 'ACTIVE' AND b.endDate < :today")
+    void updateCompletedBookings(LocalDate today);
 }
