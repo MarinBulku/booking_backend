87f58200d7669273db52ff7cdaefb29b8fbaf699
MarinBulku <marinbulku4@gmail.com>
2024-06-25 16:16:12 +0200

    Implemented the method logic to find the available rooms.

--- src/main/java/com/algorhythm/booking_backend/services/implementations/RoomServiceImpl.java
+++ src/main/java/com/algorhythm/booking_backend/services/implementations/RoomServiceImpl.java
@@ -1,21 +1,26 @@
 package com.algorhythm.booking_backend.services.implementations;
 
+import com.algorhythm.booking_backend.dataproviders.Booking.RoomSearchRequest;
+import com.algorhythm.booking_backend.dataproviders.Room.AvailableRoomDto;
+import com.algorhythm.booking_backend.dataproviders.Room.DatePrice;
 import com.algorhythm.booking_backend.dataproviders.Room.RoomCreationRequest;
-import com.algorhythm.booking_backend.entities.Hotel;
-import com.algorhythm.booking_backend.entities.Room;
+import com.algorhythm.booking_backend.entities.*;
 import com.algorhythm.booking_backend.exceptions.EntityNotFoundException;
 import com.algorhythm.booking_backend.exceptions.ImageTooLargeException;
 import com.algorhythm.booking_backend.exceptions.IncorrectFileTypeException;
-import com.algorhythm.booking_backend.repositories.HotelRepository;
-import com.algorhythm.booking_backend.repositories.RoomRepository;
+import com.algorhythm.booking_backend.repositories.*;
 import com.algorhythm.booking_backend.services.interfaces.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +28,9 @@ public class RoomServiceImpl implements RoomService {
 
     private final RoomRepository roomRepository;
     private final HotelRepository hotelRepository;
-
+    private final DiscountDateRepository discountDateRepository;
+    private final PointRepository pointRepository;
+    private final UserRepository userRepository;
     @Override
     public List<Room> findAll() {
         List<Room> allRooms = roomRepository.findAll();
@@ -32,8 +39,113 @@ public class RoomServiceImpl implements RoomService {
     }
 
     @Override
-    public List<Room> findAvailableRoomsToBook(Date startDate, Date endDate, Integer adultNumber, Integer kidNumber) {
-        return null;
+    public Page<AvailableRoomDto> findAvailableRoomsToBook(RoomSearchRequest request, Integer pageNo) {
+        if (!request.getStartDate().isBefore(request.getEndDate()))
+            throw new IllegalArgumentException("Start date must be before end date");
+
+        Optional<User> userOptional = userRepository.findById(request.getUserId());
+        if (userOptional.isEmpty())
+            throw new EntityNotFoundException("No user with this id: " + request.getUserId());
+
+        final int pageSize = 18;
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Optional<Hotel> optionalHotel = hotelRepository.findById(request.getHotelId());
+
+        if (optionalHotel.isEmpty()) throw new EntityNotFoundException("No hotel with this id: " + request.getHotelId());
+
+        Page<Room> foundRooms = roomRepository.findAvailableRooms(
+                optionalHotel.get(),
+                request.getStartDate(),
+                request.getEndDate(),
+                request.getAdultsCapacity(),
+                request.getKidsCapacity(),
+                pageable
+                );
+
+        List<AvailableRoomDto> availableRoomDto = foundRooms.stream().map(room -> {
+            try {
+                String roomImage = Base64.getEncoder().encodeToString(
+                        FileUtils.readFileToByteArray(new File(room.getRoomImagePath()))
+                );
+                ArrayList<DatePrice> datePrices = getRoomsDatePriceList(room, request.getStartDate(), request.getEndDate());
+                Double totalPrice = datePrices.stream().mapToDouble(DatePrice::getPrice).sum();
+                Double pointDiscount = getPointDiscountByRoom(room, userOptional.get());
+                return AvailableRoomDto.builder()
+                        .roomId(room.getRoomId())
+                        .roomName(room.getRoomName())
+                        .description(room.getDescription())
+                        .roomImage(roomImage)
+                        .datePriceList(datePrices)
+                        .totalPrice(totalPrice * pointDiscount)
+                        .pointsDiscount(pointDiscount)
+                        .build();
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read room image file");
+            }
+        }).toList();
+
+        ArrayList<AvailableRoomDto> list = new ArrayList<>(availableRoomDto);
+
+        if ("+".equals(request.getSort())) {
+            list.sort(Comparator.comparingDouble(AvailableRoomDto::getTotalPrice));
+        } else if ("-".equals(request.getSort())) {
+            list.sort(Comparator.comparingDouble(AvailableRoomDto::getTotalPrice).reversed());
+        }
+
+        return new PageImpl<>(list, pageable, list.size());
+    }
+
+    private Double getPointDiscountByRoom(Room room, User user) {
+
+        Integer userPoints = user.getBookingPoints();
+        List<Points> discountPoints = pointRepository.findByRoomId(room.getRoomId());
+        Double discount = 1.0;
+        for (Points p:
+             discountPoints) {
+            if (userPoints > p.getRequiredPoints())
+                discount = p.getDiscount();
+        }
+
+        return discount;
+    }
+
+    private ArrayList<DatePrice> getRoomsDatePriceList(Room room, LocalDate startDate, LocalDate endDate) {
+
+        if (!roomRepository.existsById(room.getRoomId()))
+            throw new EntityNotFoundException("No room found with this id: " + room.getRoomId());
+
+        List<DiscountDate> discountDates = discountDateRepository.findByDiscountDateBetweenAndRoom_RoomId(startDate, endDate, room.getRoomId());
+
+        Map<LocalDate, Double> discountMap = discountDates.stream()
+                .collect(Collectors.toMap(DiscountDate::getDiscountDate, DiscountDate::getDiscount));
+
+        List<LocalDate> allDates = startDate.datesUntil(endDate.plusDays(1)).toList();
+        ArrayList<DatePrice> datePrices = new ArrayList<>(allDates.size());
+
+        for (LocalDate date : allDates) {
+            Double discount = discountMap.get(date);
+            double price;
+
+            if (discount != null) {
+                price = room.getPrice() * discount;
+            } else {
+                price = switch (date.getDayOfWeek()) {
+                    case MONDAY, TUESDAY -> room.getPrice() * 0.9;
+                    case SATURDAY, SUNDAY -> room.getPrice() * 1.1;
+                    default -> room.getPrice();
+                };
+            }
+
+            datePrices.add(
+                    DatePrice.builder()
+                            .date(date)
+                            .price(price)
+                            .build()
+            );
+        }
+
+        return datePrices;
     }
 
     @Override
