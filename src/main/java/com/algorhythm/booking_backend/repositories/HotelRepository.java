fdc271812e23fc7a4b8b74e4923545467b23a92b
MarinBulku <marinbulku4@gmail.com>
2024-07-02 16:21:05 +0200

    Modified the query ,so it returns the number of free rooms that the hotel has.

--- src/main/java/com/algorhythm/booking_backend/repositories/HotelRepository.java
+++ src/main/java/com/algorhythm/booking_backend/repositories/HotelRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,22 +14,24 @@ import java.util.List;
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
     List<Hotel> findHotelsByOwner(User owner);
 
-    @Query("select h from Hotel h " +
+    @Query("select h, count(r) as freeRooms " +
+            "from Hotel h " +
             "inner join Room r on r.hotel = h " +
             "where r.adultsCapacity = :adultsNumber and r.kidsCapacity = :kidsNumber " +
             "and not exists (" +
             "   select b from Booking b " +
             "   where b.room = r " +
             "   and (" +
-            "       (b.startDate <= :checkInDate and :checkInDate <= b.endDate) or " +
-            "       (b.startDate <= :checkOutDate and :checkOutDate <= b.endDate) or " +
-            "       (:checkInDate <= b.startDate and b.startDate <= :checkOutDate)" +
+            "       (b.startDate >= :checkInDate and :checkInDate < b.endDate) or " +
+            "       (b.startDate < :checkOutDate and :checkOutDate <= b.endDate) or " +
+            "       (:checkInDate <= b.startDate and b.startDate < :checkOutDate)" +
             "   )" +
-            ")")
-    Page<Hotel> findAvailableHotels(
-            LocalDate checkInDate,
-            LocalDate checkOutDate,
-            Integer adultsNumber,
-            Integer kidsNumber,
+            ")" +
+            "group by h")
+    Page<Object[]> findAvailableHotels(
+            @Param("checkInDate") LocalDate checkInDate,
+            @Param("checkOutDate") LocalDate checkOutDate,
+            @Param("adultsNumber") Integer adultsNumber,
+            @Param("kidsNumber") Integer kidsNumber,
             Pageable pageable);
 }
