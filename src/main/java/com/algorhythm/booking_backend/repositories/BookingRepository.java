b9af46cf6bee28e9bb4e3d88e4ea05aa4c4fca2f
MarinBulku <marinbulku4@gmail.com>
2024-07-02 16:18:15 +0200

    New method to cancel bookings.

--- src/main/java/com/algorhythm/booking_backend/repositories/BookingRepository.java
+++ src/main/java/com/algorhythm/booking_backend/repositories/BookingRepository.java
@@ -2,6 +2,7 @@ package com.algorhythm.booking_backend.repositories;
 
 import com.algorhythm.booking_backend.dataproviders.Booking.BookingHistoryDto;
 import com.algorhythm.booking_backend.entities.Booking;
+import com.algorhythm.booking_backend.entities.Room;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
@@ -21,4 +23,7 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     @Transactional
     @Query("UPDATE Booking b SET b.status = 'COMPLETE' WHERE b.status = 'ACTIVE' AND b.endDate < :today")
     void updateCompletedBookings(LocalDate today);
+
+    @Query("SELECT B FROM Booking B WHERE B.room = :room AND ((B.startDate BETWEEN :startDate AND :endDate) OR (B.endDate BETWEEN :startDate AND :endDate)) AND B.status != 'CANCELLED'")
+    Optional<Booking> isThereABooking(Room room, LocalDate startDate, LocalDate endDate);
 }
