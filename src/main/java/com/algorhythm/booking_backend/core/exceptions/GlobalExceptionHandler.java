d7252219e489c75a7d8abccb3605bfe814c826da
Marin Bulku <marinbulku4@gmail.com>
2024-09-18 11:01:47 +0200

    Added server side error responses

--- src/main/java/com/algorhythm/booking_backend/core/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/algorhythm/booking_backend/core/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
@@ -21,17 +22,30 @@ public class GlobalExceptionHandler {
             IncorrectFileTypeException.class,
             MethodArgumentNotValidException.class,
             EntityExistsException.class,
-            Exception.class
     })
-    public ResponseEntity<ApiException> handleNotFoundExceptions(Exception e) {
+    public ResponseEntity<ApiExc> handleNotFoundExceptions(Exception e) {
 
 
-        ApiException exceptionToBeThrown = new ApiException(
+        ApiExc exceptionToBeThrown = new ApiExc(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
 
         return new ResponseEntity<>(exceptionToBeThrown, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {
+            IOException.class,
+            RuntimeException.class,
+    })
+    public ResponseEntity<ApiExc> handleServerExceptions(Exception e) {
+        ApiExc exception = new ApiExc(
+                e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
