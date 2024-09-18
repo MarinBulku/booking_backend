c35919e605cb4da2cd6096ea37eb8582bb03550b
Marin Bulku <marinbulku4@gmail.com>
2024-09-18 10:57:27 +0200

    Removed unnecessary semicolon

--- src/main/java/com/algorhythm/booking_backend/core/exceptions/ImageTooLargeException.java
+++ src/main/java/com/algorhythm/booking_backend/core/exceptions/ImageTooLargeException.java
@@ -2,5 +2,5 @@ package com.algorhythm.booking_backend.core.exceptions;
 
 public class ImageTooLargeException extends RuntimeException{
 
-    public ImageTooLargeException(String message){super(message);};
+    public ImageTooLargeException(String message){super(message);}
 }
